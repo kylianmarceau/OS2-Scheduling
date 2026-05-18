acf442e22dcba938fc28e86edd74c2eb66402178
Kylian Dabancourt <kyliandabancourt@Kylians-MacBook-Air.local>
2026-05-18 08:55:38 +0200

    extra seed for stronger statistcis

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 patron_counts=(10 20 30 40 50 60 70 80 90 100)
 schedulers=(0 1 2 3)
-seeds=(42 123 456 789 1337)
+seeds=(42 123 456 789 1337 2000)
 switch_time=5
 
 make compile
